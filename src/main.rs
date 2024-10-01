5718332f4148b5cbb614d1bc455c547f22af70ec
Torben Clasen <t.clasen@outlook.com>
2024-10-01 17:07:43 +0200

    Enable multiple rolls

--- src/main.rs
+++ src/main.rs
@@ -209,6 +209,39 @@ fn get_chosen_die<'a>(state: &'a mut State<'a>) -> &'a Die<'a> {
     }
 }
 
+fn setup_random() -> Rand32 {
+    let mut bytes: [u8; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
+    getrandom(&mut bytes).expect("Should be able to get random bytes");
+    Rand32::new(u64::from_be_bytes(bytes))
+}
+
+fn roll_again() -> bool {
+    println!("Do you want to roll again?(y/n)");
+
+    let mut user_input = String::new();
+    let stdin = std::io::stdin();
+    let mut handle = stdin.lock();
+
+    handle
+        .read_line(&mut user_input)
+        .expect("Failed reading from stdin!");
+    loop {
+        let trimmed = user_input.trim();
+
+        if trimmed == "y" || trimmed == "Y" {
+            return true;
+        } else if trimmed == "n" || trimmed == "N" {
+            return false;
+        } else {
+            println!("Your input matched with none of the existing dice. Please try again!");
+            user_input.clear();
+            handle
+                .read_line(&mut user_input)
+                .expect("Failed reading from stdin!");
+        }
+    }
+}
+
 fn main() {
     let mut state = State::default();
     setup_default_dice(&mut state);
@@ -221,9 +254,13 @@ fn main() {
     println!("You selected: {}", chosen_die);
     println!("Throwing the die!");
 
-    let mut bytes: [u8; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
-    getrandom(&mut bytes).expect("Should be able to get random bytes");
-    let mut random = Rand32::new(u64::from_be_bytes(bytes));
-    let random_number = random.rand_range(chosen_die.get_range());
-    println!("You rolled a: {}", random_number);
+    let mut random = setup_random();
+    let die_range = chosen_die.get_range();
+    loop {
+        let random_number = random.rand_range(die_range.clone());
+        println!("You rolled a: {}", random_number);
+        if !roll_again() {
+            break;
+        }
+    }
 }
